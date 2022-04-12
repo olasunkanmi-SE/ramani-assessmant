bbf5b011f315ed96c0648dfa84f7924357f81ed8
Ola <ola@Olas-MacBook-Pro.local>
2022-04-12 22:12:42 +0800

    remove dist folder

--- dist/src/constants/validation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiQueryParams = void 0;
-var apiQueryParams;
-(function (apiQueryParams) {
-    apiQueryParams["science"] = "science";
-    apiQueryParams["tech"] = "tech";
-})(apiQueryParams = exports.apiQueryParams || (exports.apiQueryParams = {}));
-//# sourceMappingURL=validation.js.map

1ab77ceb18233acf557178422e609e7918059721
Ola <ola@Olas-MacBook-Pro.local>
2022-04-12 23:11:12 +0800

    update test cases

--- src/tests/post.spec.ts
+++ src/tests/post.spec.ts
@@ -1,6 +1,7 @@
 import { IPost } from "./../interfaces/index";
 import { NextFunction, Request, Response } from "express";
 import { PostController } from ".././controllers/post-controller";
+import { mockPosts } from "./mock-posts";
 
 describe("post controller", () => {
   let responseObject: { error: string; posts: IPost[] };
@@ -47,7 +48,30 @@ describe("post controller", () => {
       },
     } as any as Response;
 
-    await PostController.getPosts(request, response, mockNext);
+    await PostController.postQueryValidation(request, response);
     expect(responseObject.error).toBe("Tags parameter is required");
+  });
+
+  test("Should sort the array in ascending order of likes", async () => {
+    const request = {
+      query: {
+        tags: "science",
+        direction: "asc",
+        sortBy: "likes",
+      },
+    } as any as Request;
+
+    const response = {
+      json: jest.fn().mockImplementation((result) => {
+        responseObject = result;
+      }),
+      status: function (responseStatus: any) {
+        expect(responseStatus).toEqual(200);
+        return this;
+      },
+    } as any as Response;
+
+    await PostController.getPosts(request, response, mockNext);
+    expect(responseObject.posts[0].likes).toBe(31);
   });
 });
